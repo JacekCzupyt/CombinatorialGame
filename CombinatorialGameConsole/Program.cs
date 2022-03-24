5c5b91bf15d2aba76c5b3c34486b27b24dceec3d
JacekCzupyt <kuba.czupyt@gmail.com>
2022-03-24 13:00:25 +0100

    Initial commit

---
+++ CombinatorialGameConsole/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace CombinatorialGameConsole {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ CombinatorialGameLibrary/Class1.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+namespace CombinatorialGameLibrary {
+    public class Class1 {
+    }
+}
