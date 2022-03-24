cc65ce335f764c5c32b6277fcf6a76b5ae633a0b
JacekCzupyt <kuba.czupyt@gmail.com>
2022-03-24 14:08:14 +0100

    Implement game state interface

---
+++ CombinatorialGameLibrary/IGameState.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CombinatorialGameLibrary {
+    public interface IGameState : ICloneable {
+        int N { get; }
+        int K { get; }
+        List<int> GameList { get; }
+        int? MakeMove(int m, int c);
+    }
+}
